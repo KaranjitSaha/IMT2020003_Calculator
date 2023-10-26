07bd71b8332374ed04ef21c423094e03174f2f01
KaranjitSaha <Karanjit.Saha@iiitb.ac.in>
2023-10-26 12:30:13 +0530

    calculator v1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,54 @@
-package org.example;
+// package org.example;
+
+import java.util.Scanner;
 
 public class Main {
+
+    public static int add(int num1,int num2){
+        return num1+num2;
+    }
+
+    public static int subtract(int num1,int num2){
+        return num1-num2;
+    }
+
+    public static int multiply(int num1,int num2){
+        return num1*num2;
+    }
+
+    public static int division(int num1,int num2){
+        return num1/num2;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Please select the operation from below: ");
+        System.out.println("1. Add ");
+        System.out.println("2. Subtract ");
+        System.out.println("3. Multiply ");
+        System.out.println("4. Division ");
+        
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        
+        System.out.println("Enter Choice: ");
+        int choice = myObj.nextInt();  // Read user input
+
+        System.out.println("Enter the 2 numbers: ");
+        int num1 = myObj.nextInt();  // Read user input
+        int num2 = myObj.nextInt();  // Read user input   
+        int ans=0;
+        switch(choice) {
+            case 1:
+                ans=add(num1,num2);
+                break;
+            case 2:
+                ans=subtract(num1,num2);
+                break;
+            case 3:
+                ans=multiply(num1,num2);
+                break;
+            case 4:
+                ans=division(num1,num2);
+                break;
+          }
+        System.out.println("The answer is = "+ans);
     }
 }
